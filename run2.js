9c0ce2238ee9b4022a35c1f1f828426453fac4c3
tl <>
2021-12-31 13:28:21 +0800

    Update

--- run2.js
+++ run2.js
@@ -1,31 +1,60 @@
 const { chromium } = require('playwright');
+const axios = require('axios');
+
+const { exec } = require("child_process");
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+var fs = require('fs');
+const request = require('request')
+const download = (url, path, callback) => {
+  request.head(url, (err, res, body) => {
+    request(url)
+      .pipe(fs.createWriteStream(path))
+      .on('close', callback)
+  })
+}
 
-async function handleInnerItem(inner, context) {
+
+
+async function handleInnerItem(inner, context, parent_title) {
   var parent = await inner.$('xpath=..');
   var title = (await parent.innerText()).split("\n")[1];
   await inner.click();
-  await sleep(2000);
+  await sleep(5000);
   var page = context.pages()[context.pages().length - 1];
-  await page.click('text="下载音频"');
-  await sleep(2000);
-  var page_audio = context.pages()[context.pages().length - 1];
-  var url = await page_audio.url()
-  console.log(title + "|" + url)
-  await page_audio.close();
-  await page.close();
-
+  let url = await page.url();
+  page.goto(url);
+  var video_json_url = '';
+  page.on('request', request => {
+    var req_url = request.url();
+    if (req_url.indexOf("download?csrf_token") != -1) {
+      video_json_url = req_url;
+    }
+  });
+  for (var i = 0; i < 10; i++) {
+    await sleep(5000)
+    if (video_json_url.indexOf("http")!=-1) {
+      await page.goto(video_json_url);
+      var pre = await page.$("pre");
+      var text = await pre.innerText();
+      var parsed = JSON.parse(text);
+      var videoUrl = parsed.data.videoUrl
+      console.log(title + "|" + videoUrl);
+      download(videoUrl, "kuaiji_downloaded/" + parent_title + "-" + title + ".mp4", function() { });
+      break;
+    }
+  }
+  page.close()
 }
 
-async function handleItem(item, context) {
+async function handleItem(item, context, parent_title) {
   var parent = (await item.$('xpath=..'));
   var title = (await parent.innerText()).split("\n")[0];
   await item.click();
-  await sleep(2000);
+  await sleep(5000);
   var pages = context.pages()
   var inner_page = pages[pages.length - 1];
 
@@ -36,7 +65,7 @@ async function handleItem(item, context) {
   }
   for (var i = 0; i < items_inner.length; i++) {
     var item_inner = items_inner[i];
-    await handleInnerItem(item_inner, context);
+    await handleInnerItem(item_inner, context, parent_title + "-" + title);
   }
   await inner_page.close();
 
@@ -71,7 +100,7 @@ async function handleItem(item, context) {
       console.log(items.length + " items found");
       for (var j = 0; j < items.length; j++) {
         var item = items[j];
-        await handleItem(item, context);
+        await handleItem(item, context, sessions[i] + "-" + years[k]);
       }
     }
   }
